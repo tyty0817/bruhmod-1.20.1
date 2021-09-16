8f0fa75e6565f7d144dcd8f9f874bcd67ec42454
Justin Dekker <justindekker@att.net>
2021-09-15 17:50:58 -0700

    add sconce

--- src/main/java/com/github/donotdoughnut/bruhmod/BruhMod.java
+++ src/main/java/com/github/donotdoughnut/bruhmod/BruhMod.java
@@ -1,12 +1,16 @@
 package com.github.donotdoughnut.bruhmod;
 
-import com.github.donotdoughnut.bruhmod.config.ModConfig;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.github.donotdoughnut.bruhmod.blocks.Sconce;
+import com.github.donotdoughnut.bruhmod.config.ModConfig;
+
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -24,7 +28,9 @@ public class BruhMod implements ModInitializer {
 		ModConfig.register();
 
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "test_item"), new Item(new FabricItemSettings().group(ItemGroup.MISC)));
-
+		Sconce sconce=new Sconce(FabricBlockSettings.of(Material.WOOD));
+Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "sconce"), sconce);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "sconce"), new BlockItem(sconce, new FabricItemSettings().group(ItemGroup.DECORATIONS)));
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
