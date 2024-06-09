4f08d39e7413542936120ceb7b79786e59f07cd5
Rhys Holloway <rhys@rhys.uno>
2024-06-09 15:10:43 -0700

    Fix maelstrom

--- src/main/java/name/bruhmod/items/Maelstrom.java
+++ src/main/java/name/bruhmod/items/Maelstrom.java
@@ -1,7 +1,5 @@
 package name.bruhmod.items;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -9,10 +7,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class Maelstrom extends Item {
@@ -23,21 +18,12 @@ public class Maelstrom extends Item {
     @Override
     public TypedActionResult<ItemStack> use (World world, PlayerEntity user, Hand hand) {
         user.getItemCooldownManager().set(this, 50);
-        if(!world.isClient) {
-
-            HitResult hit = user.raycast(1000, 1, true);
-            if(hit.getType() == HitResult.Type.BLOCK) {
-                BlockHitResult blockHit = (BlockHitResult) hit;
-                BlockPos blockPos = blockHit.getBlockPos();
-                BlockState blockState = world.getBlockState(blockPos);
-
-                Entity lighting_bolt = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
-                Vec3d pos = blockPos.toCenterPos();
-                lighting_bolt.setPos(pos.x, pos.y, pos.z);
-                world.spawnEntity(lighting_bolt);
-            }
+        HitResult hit = user.raycast(1000, 1, true);
+        if(hit.getType() != HitResult.Type.MISS) {
+            LightningEntity lightningEntity = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
+            lightningEntity.setPosition(hit.getPos());
+            world.spawnEntity(lightningEntity);
         }
-
         user.getStackInHand(hand).damage(1, user, playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
