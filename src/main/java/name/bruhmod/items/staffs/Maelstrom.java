6cf30ec0cd701c45cafaa3fd69964413beaf6718
Tyler <bigdogty9@gmail.com>
2024-07-07 04:37:08 -0700

    Maelstrom can only strike during rain

--- src/main/java/name/bruhmod/items/staffs/Maelstrom.java
+++ src/main/java/name/bruhmod/items/staffs/Maelstrom.java
@@ -1,6 +1,7 @@
 package name.bruhmod.items.staffs;
 
 import name.bruhmod.Mod;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LightningEntity;
@@ -11,6 +12,7 @@ import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -30,14 +32,17 @@ public class Maelstrom extends Item {
 
     @Override
     public TypedActionResult<ItemStack> use (World world, PlayerEntity user, Hand hand) {
-        user.getItemCooldownManager().set(this, 50);
         HitResult hit = user.raycast(128, 1, true);
-        if(hit.getType() != HitResult.Type.MISS) {
+        BlockPos pos = new BlockPos((int) hit.getPos().x, (int) hit.getPos().y, (int) hit.getPos().z);
+        boolean canRain = world.getBiome(pos).streamTags()
+                .noneMatch(a -> a == ConventionalBiomeTags.DESERT || a == ConventionalBiomeTags.MESA || a == ConventionalBiomeTags.SAVANNA);
+        if((world.isRaining() || world.isThundering()) && canRain && hit.getType() != HitResult.Type.MISS) {
+            user.getItemCooldownManager().set(this, 50);
             LightningEntity lightningEntity = new LightningEntity(EntityType.LIGHTNING_BOLT, world);
             lightningEntity.setPosition(hit.getPos());
             world.spawnEntity(lightningEntity);
+            user.getStackInHand(hand).damage(1, user, playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
         }
-        user.getStackInHand(hand).damage(1, user, playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
         return TypedActionResult.pass(user.getStackInHand(hand));
     }
 }
